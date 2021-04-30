38a513ad0d037f3bc6024c8dece9e6809e6fc52b
Wisarut Multhonggad <wisarut.multhonggad@gmail.com>
2021-05-01 00:12:42 +0700

    Initial commit

---
+++ bitnance assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitnance assignment
+//
+//  Created by Wisarut Multhonggad on 5/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
